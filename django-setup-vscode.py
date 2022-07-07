dd9322285e7fa9e07c1d887c48616fe5b6a28da1
Stephen <stephen@gilmore.us>
2022-07-06 21:37:51 -0700

    needs to be tested

--- django-setup-vscode.py
+++ django-setup-vscode.py
@@ -12,7 +12,7 @@ class CreateGitHubRepository(object):
         steps = [
             "Log into GitHub",
             "Create a new project",
-            "Name project, include README and .gitignore for python",
+            f"Name project '{context['project_name']}' include README and .gitignore for python",
         ]
         print_steps(steps=steps)
 
@@ -42,18 +42,6 @@ class CreateVenv(object):
         print_steps(steps=steps)
 
 
-class RunCookieCutter(object):
-    def run(self, context):
-        steps = [
-            "Press cmd+shift+P",
-            "Select 'Python: Create Terminal'",
-            "Double check for (.venv)",
-            "Run 'pip install cookiecutter",
-            "Run 'cookiecutter https://github.com/feldroy/django-crash-starter.git'",
-        ]
-        print_steps(steps=steps)
-
-
 class PipInstallRequirements(object):
     def run(self, context):
         steps = [
@@ -62,8 +50,17 @@ class PipInstallRequirements(object):
             "Double check for (.venv)",
             "Run 'pip install -r requirements.txt",
             "Requirements should include....",
-            "Django, python-dotenv, black ",
-            "If installed manually, run 'pip freeze requirements.txt'",
+            "Django, black, python-dotenv",
+            "Run 'pip freeze requirements.txt'",
+        ]
+        print_steps(steps=steps)
+
+
+class GenerateDjangoProject(object):
+    def run(self, context):
+        steps = [
+            f"Run 'django-admin startproject {context['project_name']} .'",
+            "the '.' is crucial to telling the script to install in the current directory",
         ]
         print_steps(steps=steps)
 
@@ -71,19 +68,72 @@ class PipInstallRequirements(object):
 class DotEnv(object):
     def run(self, context):
         steps = [
-            "TODO: Set up .env for the SECRET_KEY and any other important env variables",
+            ".env Updates:",
+            "Open settings.py",
+            "Add 'from dotenv import load_dotenv'",
+            "Add 'load_dotenv()",
+            "Move the SECRET_KEY to a .env file in root",
+            "Update settings.py SECRET_KEY=os.getenv('SECRET_KEY')",
+        ]
+        print_steps(steps=steps)
+
+
+class SettingsConfig(object):
+    def run(self, context):
+        steps = [
+            "Change settings.py",
+            "Add 'import os'",
+            "TIME_ZONE = 'America/Los_Angeles",
+            "LANGUAGE_CODE = 'en-us",
+            "STATIC_URL = '/static/",
+            "STATIC_ROOT = BASE_DIR / 'static'",
+            "ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']",
+        ]
+        print_steps(steps=steps)
+
+
+class RunMigrations(object):
+    def run(self, context):
+        steps = [
+            "Create the database, 'python manage.py migrate'",
+        ]
+        print_steps(steps=steps)
+
+
+class VSCodeDebugLaunchFile(object):
+    def run(self, context):
+        steps = [
+            "Navigate to debug window and select 'create a launch.json file",
+            "Option: Django",
+            "Try Running the debug and visiting 127.0.0.1:8000",
+        ]
+        print_steps(steps=steps)
+
+
+class RunWebServer(object):
+    def run(self, context):
+        steps = [
+            "Start the web server",
+            "Run 'python manage.py runserver'",
         ]
         print_steps(steps=steps)
 
 
 if __name__ == "__main__":
-    context = {}
+    context = {
+        "project_name": input("Enter a project name: ") or "<project_name>",
+    }
     procedure = [
         CreateGitHubRepository(),
         CloneRepository(),
         CreateVenv(),
         PipInstallRequirements(),
-        DotEnv(),  # TODO
+        GenerateDjangoProject(),
+        DotEnv(),
+        SettingsConfig(),
+        RunMigrations(),
+        VSCodeDebugLaunchFile(),
+        RunWebServer(),
     ]
     for step in procedure:
         step.run(context)
